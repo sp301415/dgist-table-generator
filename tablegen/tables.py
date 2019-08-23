cc4861a0d5068051bc3cb8f27d4c3d68ce02a1af
In Tak Hwang <sp301415@gmail.com>
2019-08-23 12:39:34 +0900

    Minor adjustments

--- tablegen/tables.py
+++ tablegen/tables.py
@@ -9,14 +9,14 @@ class Tables:
             self.courses.append([c for c in courses if c.title == t])
 
         all_tables = product(*self.courses)
-        self.possible_tables = [self.arrange_table_by_day(t) for t in all_tables if self.check_courses(t)]
+        self.possible_tables = [self._arrange_table_by_day(t) for t in all_tables if self.check_courses(t)]
         assert len(self.possible_tables) >= 1
 
         self.courses_merged = [c[0] for c in self.courses]
         self.credits = sum(c.credit for c in self.courses_merged)
 
     @staticmethod
-    def check_overlap(c_time1, c_time2):
+    def _check_overlap(c_time1, c_time2):
         for day in "월화수목금":
             try:
                 time1 = [float(i) for i in c_time1[day].replace(":", ".").split("-")]
@@ -31,7 +31,7 @@ class Tables:
         return False
 
     @staticmethod
-    def arrange_table_by_day(table):
+    def _arrange_table_by_day(table):
         # rearrange tables by day
         table_day = defaultdict(list)
 
@@ -47,11 +47,10 @@ class Tables:
 
         return dict(table_day)
 
-    @classmethod
-    def check_courses(cls, courses):
+    def check_courses(self, courses):
         # choose any two courses and check whether it overlaps or not
         for i in combinations([c.time for c in courses], 2):
-            if cls.check_overlap(*i):
+            if self._check_overlap(*i):
                 return False
         return True
 
